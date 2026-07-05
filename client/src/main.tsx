7690096dee3330f27ee63be8192f41948c3aac11
Osama Jarrar <osamajarrar8@gmail.com>
2026-07-05 13:05:41 -0400

    Add PostHog analytics alongside GA4

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,9 @@
 import "./lib/i18n";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { initAnalytics } from "./lib/analytics";
 import "./index.css";
+
+initAnalytics();
 
 createRoot(document.getElementById("root")!).render(<App />);
